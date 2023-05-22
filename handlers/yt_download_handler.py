d312365e635834e506c198be7e2682196519ab68
hakunekooo <hakunekooo@proton.me>
2023-05-23 05:40:44 +0600

    add duplicate files checker & unique_id bug fixes

--- handlers/yt_download_handler.py
+++ handlers/yt_download_handler.py
@@ -4,25 +4,21 @@ from handlers.status_handler import print_status
 from utils import utils
 
 def yt_download_handler(config, download_urls):
-    # update download_history.txt file from
-    # looking through video id of file names
-    # adding them in download_history.txt
-    # renaming every file using create_next_unique_id()
-    #TODO:
-#   update_download_history()
-
     query_type = config['query_type']
+    download_format = config['format']
+    download_directory = config['directory']
+    download_history_txt_path = f"{download_directory}/download_history.txt"
+        
+    utils.update_download_history(download_format, download_directory, download_history_txt_path)
 
     print_status(f"Downloading {query_type} - Total {len(download_urls)} Queries")
 
     for url in download_urls:
-        download_options = create_download_options(config)
+        download_options = create_download_options(download_directory, download_format)
         with YoutubeDL(download_options) as ytdlp:
             ytdlp.download(url)
 
-def create_download_options(config):
-    download_format = config['format']
-    download_directory = config['directory']
+def create_download_options(download_directory, download_format):
 
 
     download_options = {
@@ -37,6 +33,7 @@ def create_download_options(config):
     with open('./sources/source.yml', 'r') as file:
         yml_config = yaml.safe_load(file)
         media_type = yml_config['media_type']
+
     if media_type == 'Music': download_options['format'] = 'bestaudio/best'
     
     return download_options
--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,5 @@
 import os, re
+from handlers.status_handler import print_status
 
 # Generates a template for the output file name based on the given directory and file format.
 # Returns: str: The generated template for the output file name.
@@ -26,7 +27,7 @@ def create_next_unique_id(directory, file_format):
     for file_name in os.listdir(directory):
         if file_name.endswith(f".{file_format}"):
             result = extract_file_info(file_name)
-            unique_id = result[0]
+            unique_id = result['unique_id']
             existing_ids.append(unique_id)
 
     next_id = max(existing_ids) + 1 if existing_ids else 0
@@ -47,6 +48,59 @@ def extract_file_info(file_name):
         unique_id = int(matches[0])
         title = matches[1]
         video_id = matches[2]
-        return [unique_id, title, video_id]
+        return {
+            'unique_id': unique_id,
+            'title': title,
+            'video_id': video_id
+        }
     else:
         raise ValueError(f"file naming is inconsistent: {file_name}")
+    
+# update download_history.txt file from
+# looking through video id of file names
+# adding them in download_history.txt
+# renaming every file if unique_id got messed up!
+def update_download_history(file_extension, download_directory, download_history_txt_path):
+
+    print_status(f"Checking Unique IDs From {file_extension} files", delay=2)
+
+    fix_unique_ids(download_directory, file_extension)
+
+    print_status(f"Updating Download History: {download_history_txt_path}")
+
+    if not os.path.isfile(download_history_txt_path):
+        print_status("No Previous Download History Found!")
+    else:
+        video_ids = []
+        for file_name in os.listdir(download_directory):
+            if file_name.endswith(file_extension):
+                result = extract_file_info(file_name)
+                video_id = result["video_id"]
+                video_ids.append(f"youtube {video_id}")
+
+        with open(download_history_txt_path, 'w') as file:
+            for video_id in video_ids:
+                file.write(video_id + "\n")
+
+        print_status(f"Updated Download History!")
+
+# fix unique ids from files in a directory
+# by renaming them if nessesary
+def fix_unique_ids(directory, file_extension):
+    all_files = [file for file in os.listdir(directory) if file.endswith(file_extension)]
+    all_files.sort()
+
+    expected_id = 0
+    for file in all_files:
+        result = extract_file_info(file)
+        current_id = result["unique_id"]
+
+        if current_id != expected_id:
+            print_status(f"Warning: Unique IDs are not Sorted!", color="warning")
+
+            title = result["title"]
+            video_id = result["video_id"]
+            new_file_name = f"[{expected_id}] [{title}] [{video_id}].{file_extension}"
+            os.rename(os.path.join(directory, file), os.path.join(directory, new_file_name))
+
+        expected_id += 1
